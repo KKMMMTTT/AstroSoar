c5ea68dc3f25aeec2e6995702222ffbcb5c95858
Matthew Chrobak <MatthewChrobak@hotmail.com>
2020-06-01 21:55:34 -0400

    Project init

---
+++ source/Program.cs
@@ -0,0 +1,13 @@
+﻿using Annex;
+using Game.Scenes;
+
+namespace Game
+{
+    public static class Program
+    {
+        private static void Main(string[] args) {
+            AnnexGame.Initialize();
+            AnnexGame.Start<MainMenu>();
+        }
+    }
+}
