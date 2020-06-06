6970988b0839c043d4efc4440043ec26e63a877e
Matthew Chrobak <MatthewChrobak@hotmail.com>
2020-06-05 20:27:34 -0400

    Added questline definition structure

---
+++ source/Definitions/Questlines/QuestlineDefinition.cs
@@ -0,0 +1,18 @@
+﻿using Game.Definitions;
+using Game.Definitions.Questlines;
+using static Game.Definitions.Strings;
+
+namespace Game.Questlines
+{
+    public class QuestlineDefinition : IDescribable
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string FullDescription => this.Description ?? NoDescription;
+
+        /// <summary>
+        /// Array of steps to be completed in linear order
+        /// </summary>
+        public StepDefinition[] Steps { get; set; }
+    }
+}
---
+++ source/Definitions/Questlines/StepDefinition.cs
@@ -0,0 +1,15 @@
+﻿using static Game.Definitions.Strings;
+
+namespace Game.Definitions.Questlines
+{
+    public class StepDefinition : IDescribable
+    {
+        public string? Description { get; set; }
+        public string FullDescription => this.Description ?? NoDescription;
+
+        /// <summary>
+        /// Array of possible tasks to complete before completing the step
+        /// </summary>
+        public TaskDefinition[] Task { get; set; }
+    }
+}
