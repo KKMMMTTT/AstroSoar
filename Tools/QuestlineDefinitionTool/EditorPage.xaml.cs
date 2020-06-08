b7e0e1c399d5fceaa718e2936e071c0bdb4117a7
Matthew Chrobak <MatthewChrobak@hotmail.com>
2020-06-07 23:31:58 -0400

    Progress

---
+++ Tools/QuestlineDefinitionTool/EditorPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Game.Definitions.Questlines;
+using Game.Questlines;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuestlineDefinitionTool
+{
+    /// <summary>
+    /// Interaction logic for EditorPage.xaml
+    /// </summary>
+    public partial class EditorPage : Window
+    {
+        private StepDefinition CurrentlySelectedStep => this.lstSteps.SelectedIndex == -1 ? null : this.Steps[this.lstSteps.SelectedIndex];
+        private QuestlineDefinition QuestlineDefinition;
+        private StepDefinition[] Steps {
+            get => this.QuestlineDefinition.Steps;
+            set => this.QuestlineDefinition.Steps = value;
+        }
+
+        public EditorPage(QuestlineDefinition questlineDefinition) {
+            InitializeComponent();
+            this.QuestlineDefinition = questlineDefinition;
+
+            if (this.QuestlineDefinition.Steps == null) {
+                this.QuestlineDefinition.Steps = new StepDefinition[0];
+            }
+
+            this.txtName.Text = questlineDefinition.Name;
+            this.txtDescription.Text = questlineDefinition.Description;
+
+            RefreshStepList();
+        }
+
+        private void LstSteps_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var step = this.CurrentlySelectedStep;
+            if (step == null) {
+                return;
+            }
+            this.txtStepDescription.Text = step.Description;
+
+            foreach (var task in step.Task) {
+                this.grdTasks.Items.Add(task);
+            }
+
+            if (this.CurrentlySelectedStep != null) {
+                this.grdStepEditor.IsEnabled = true;
+            } else {
+                this.grdStepEditor.IsEnabled = false;
+            }
+        }
+
+        private void RefreshStepList() {
+            this.lstSteps.Items.Clear();
+
+            for (int i = 0; i < this.Steps.Length; i++) {
+                this.lstSteps.Items.Add(new StepListboxItem(this.Steps[i], i));
+            }
+        }
+
+        private void txtName_TextChanged(object sender, TextChangedEventArgs e) {
+            QuestlineDefinition.Name = txtName.Text;
+        }
+
+        private void txtDescription_TextChanged(object sender, TextChangedEventArgs e) {
+            QuestlineDefinition.Description = txtDescription.Text;
+        }
+
+        private void txtStepDescription_TextChanged(object sender, TextChangedEventArgs e) {
+            var step = this.CurrentlySelectedStep;
+            if (step == null) {
+                return;
+            }
+            step.Description = this.txtStepDescription.Text;
+            (this.lstSteps.SelectedItem as StepListboxItem).RefreshLabel(this.lstSteps.SelectedIndex);
+        }
+
+        private void cmdAdd_Click(object sender, RoutedEventArgs e) {
+            var newSteps = new StepDefinition[this.Steps.Length + 1];
+            for (int i = 0; i < this.Steps.Length; i++) {
+                newSteps[i] = this.Steps[i];
+            }
+            newSteps[^1] = new StepDefinition() { Task = new TaskDefinition[0] };
+            this.QuestlineDefinition.Steps = newSteps;
+            RefreshStepList();
+        }
+
+        private void cmdRemove_Click(object sender, RoutedEventArgs e) {
+            if (this.CurrentlySelectedStep == null) {
+                return;
+            }
+
+            if (MessageBox.Show("Are you sure you want to delete this step?", "Delete Step", MessageBoxButton.YesNo, MessageBoxImage.Warning) != MessageBoxResult.Yes) {
+                return;
+            }
+
+            var newSteps = new StepDefinition[Math.Max(this.Steps.Length - 1, 0)];
+            int bad = this.lstSteps.SelectedIndex;
+            int insertIndex = 0;
+
+            for (int i = 0; i < this.Steps.Length; i++) {
+                if (i == bad) {
+                    continue;
+                }
+                newSteps[insertIndex++] = this.Steps[i];
+            }
+            this.QuestlineDefinition.Steps = newSteps;
+            this.RefreshStepList();
+        }
+
+        private void cmdShiftUp_Click(object sender, RoutedEventArgs e) {
+            if (this.CurrentlySelectedStep == null) {
+                return;
+            }
+
+            int source = this.lstSteps.SelectedIndex;
+            int destination = Math.Max(this.lstSteps.SelectedIndex - 1, 0);
+
+            var temp = this.Steps[destination];
+            this.Steps[destination] = this.Steps[source];
+            this.Steps[source] = temp;
+
+            RefreshStepList();
+            this.lstSteps.SelectedIndex = destination;
+        }
+
+        private void cmdShiftDown_Click(object sender, RoutedEventArgs e) {
+            if (this.CurrentlySelectedStep == null) {
+                return;
+            }
+
+            int source = this.lstSteps.SelectedIndex;
+            int destination = Math.Min(this.lstSteps.SelectedIndex + 1, this.lstSteps.Items.Count - 1);
+
+            var temp = this.Steps[destination];
+            this.Steps[destination] = this.Steps[source];
+            this.Steps[source] = temp;
+
+            RefreshStepList();
+            this.lstSteps.SelectedIndex = destination;
+        }
+
+        public class StepListboxItem : Label
+        {
+            public StepDefinition StepDefinition;
+
+            public StepListboxItem(StepDefinition stepDefinition, int index) {
+                this.StepDefinition = stepDefinition;
+                this.RefreshLabel(index);
+            }
+
+            public void RefreshLabel(int index) {
+                this.Content = $"{index}: {StepDefinition.Description}";
+            }
+        }
+    }
+}
---
+++ Tools/QuestlineDefinitionTool/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Game.Questlines;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuestlineDefinitionTool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+
+            this.lstQuestlineDefinitions.MouseDoubleClick += this.LstQuestlineDefinitions_MouseDoubleClick;
+        }
+
+        private void LstQuestlineDefinitions_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            (this.lstQuestlineDefinitions.SelectedItem as QuestlineDefinitionListboxItem)?.OnClick();
+        }
+
+        private void RefreshQuestlineDefinitionList(int? newSelectedIndex = null) {
+            this.lstQuestlineDefinitions.Items.Clear();
+
+            foreach (var item in Globals.QuestlineDefinitions) {
+                this.lstQuestlineDefinitions.Items.Add(new QuestlineDefinitionListboxItem(item));
+            }
+
+            if (newSelectedIndex != null) {
+                this.lstQuestlineDefinitions.SelectedIndex = (int)newSelectedIndex;
+            }
+        }
+
+        private void mnuSetDefinitionsPath_Click(object sender, RoutedEventArgs e) {
+            var filedialog = new CommonOpenFileDialog();
+            filedialog.Title = "Select the Questline Definition folder";
+            filedialog.Multiselect = false;
+            filedialog.IsFolderPicker = true;
+
+            if (filedialog.ShowDialog() != CommonFileDialogResult.Ok) {
+                return;
+            }
+            if (!Directory.Exists(filedialog.FileName)) {
+                MessageBox.Show($"The directory {filedialog.FileName} does not exist!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Globals.DefinitionPath = filedialog.FileName;
+        }
+
+        private void RemoveClick(object sender, RoutedEventArgs e) {
+            if (MessageBox.Show("Are you sure you want to remove this questline definition?", "Confirm Remove", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No) {
+                return;
+            }
+            int index = this.lstQuestlineDefinitions.SelectedIndex;
+
+            if (index >= 0 && index < this.lstQuestlineDefinitions.Items.Count) {
+                Globals.QuestlineDefinitions.RemoveAt(index);
+                RefreshQuestlineDefinitionList(-1);
+            }
+        }
+
+        private void NewDefinition(object sender, RoutedEventArgs e) {
+            var newDefinition = new QuestlineDefinition() { Name = "New Questline" };
+            Globals.QuestlineDefinitions.Add(newDefinition);
+            RefreshQuestlineDefinitionList(Globals.QuestlineDefinitions.Count - 1);
+        }
+
+        private void SaveAll(object sender, RoutedEventArgs e) {
+
+        }
+
+        public class QuestlineDefinitionListboxItem : Label
+        {
+            private QuestlineDefinition Definition;
+
+            public QuestlineDefinitionListboxItem(QuestlineDefinition definition) {
+                this.Definition = definition;
+                this.Content = definition.Name;
+            }
+
+            public void OnClick() {
+                Globals.MainWindow.Hide();
+                new EditorPage(this.Definition).ShowDialog();
+                Globals.MainWindow.RefreshQuestlineDefinitionList();
+                Globals.MainWindow.ShowDialog();
+            }
+        }
+    }
+}
