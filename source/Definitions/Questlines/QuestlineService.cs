bf11f889851420a5d975db50c61b8c8532e958bf
laurie-y <31772505+laurie-y@users.noreply.github.com>
2020-06-13 22:30:23 -0400

    [#13] Added service provider for questlines

---
+++ source/Definitions/Questlines/QuestlineService.cs
@@ -0,0 +1,20 @@
+﻿using Annex;
+using Annex.Assets;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game.Definitions.Questlines
+{
+    public class QuestlineService : IService
+    {
+        public QuestlineService(){
+        }
+
+        public void Destroy(){
+        }
+
+        public IEnumerable<IAssetManager> GetAssetManagers(){
+            return Enumerable.Empty<IAssetManager>();
+        }
+    }
+}
